5fbbd5209ae7ed59bff6e0a90cf50dfa2138e4e6
oliabhi <oliabhi26@gmail.com>
2023-01-31 09:37:24 +0530

    additions to logging logic & data manipulation

--- classes/class_pyspark.py
+++ classes/class_pyspark.py
@@ -2,11 +2,16 @@
 
 # tag::import[]
 
-import json, os, re, sys, time
+import json
+import os
+import re
+import sys
+import time
 from typing import Any, Callable, Optional
 
 from pyspark.sql.dataframe import DataFrame
 from pyspark.sql import SparkSession
+from pyspark import SparkContext
 
 # end::import[]
 
@@ -18,17 +23,31 @@ class Sparkclass:
         # self.config = config
         # self.myvar  = "hello spark initiated"
 
-    def sparkStart(self, kwargs:dict):
+    def sparkStart(self, kwargs: dict):
         # print(kwargs)
         MASTER = kwargs['spark_conf']['master']
         APP_NAME = kwargs['spark_conf']['appname']
         LOG_LEVEL = kwargs['log']['level']
 
-        def createSession(master , app_name):
+        def createSession(master: Optional[str] = "local[*]", app_name: Optional[str] = "myapp") -> SparkSession:
             '''
                 Create a spark session
             '''
 
-            spark = SparkSession.builder.appName(app_name).master(master).getOrCreate()
+            spark = SparkSession.builder.appName(
+                app_name).master(master).getOrCreate()
+            return spark
+
+        def getSettings(spark: SparkSession) -> None:
             print(spark)
-        createSession(MASTER,APP_NAME)
+            print(spark.SparkContext.getConf.getAll())
+
+        def setLogging(spark: SparkSession, log_level: Optional[str] = None) -> None:
+            spark.sparkContext.setLogLevel(log_level) if log_level else None
+
+        def getLoggng(spark: SparkSession):
+            pass
+        spark = createSession(MASTER, APP_NAME)
+        getSettings(spark)
+
+        return (spark)
--- jobs/sales.py
+++ jobs/sales.py
@@ -4,14 +4,15 @@ from typing import Callable,Optional
 
 from pyspark.sql.dataframe import DataFrame
 from pyspark.sql import SparkSession
+from pyspark import SparkContext
 
 project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(project_dir)
-LOG_FILE =f"{project_dir}/logs- {os.path.basename(__file__)}.log"
-print(LOG_FILE)
+# print(project_dir)
+LOG_FILE =f"{project_dir}/logs/job-{os.path.basename(__file__)}.log"
+# print(LOG_FILE)
 LOG_FORMAT = f"%(asctime)s - LINE:%(lineno)d - %(name)s - %(levelname)s - %(funcName)s - %(message)s"
 logging.basicConfig(filename=LOG_FILE, level = logging.DEBUG , format = LOG_FORMAT)
-#logger = logging.getLogger('py4j')
+logger = logging.getLogger('py4j')
 
 sys.path.insert(1, project_dir)
 # print(sys.path)
@@ -27,7 +28,8 @@ def main(project_dir:str) -> None:
     # print(project_dir)
 
     config = openfile(f"{project_dir}/json/sales.json")
-    sparkStart(config)
+    spark = sparkStart(config)
+    sparkStop(spark)
 
 
 def openfile(filepath: str) -> dict:
@@ -55,6 +57,8 @@ def sparkStart(config:dict) -> SparkSession:
         class_pyspark.Sparkclass(conf={}).sparkStart(config)
 
 
+def sparkStop(spark:SparkSession) -> None:
+    spark.stop() if isinstance(spark, SparkSession) else None
 
 
 
